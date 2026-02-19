97eeec54c05c2156fceb425984faa9588e7135b6
Juan Pablo Maggi <juanpablomaggiv@gmail.com>
2026-02-19 01:19:26 -0300

    Update project name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: "juanpablomaggi", // Usually your GitHub org/user name.
-    projectName: "docusaurus", // Usually your repo name.
+    projectName: "juanpablomaggi.github.io", // Usually your repo name.
 
     onBrokenLinks: "throw",
 
